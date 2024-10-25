a12042eea4249c30de300ca87b11b5a48e4ab63a
Mirko Haaser <mirko@topred.media>
2024-10-25 15:12:37 +0200

    Use datetime as campicture filename

--- actions.py
+++ actions.py
@@ -1,4 +1,6 @@
+import time
 from abc import ABC
+
 from detection import Detection
 
 
@@ -14,10 +16,12 @@ class TakePictureAction(AAction):
 
     def perform(self, detection: Detection):
         try:
+            filename_prefix = time.strftime("%Y-%m-%d-%H%M%S", time.gmtime(detection.time))
+            print(filename_prefix)
             from picamzero import Camera
             cam = Camera()
             cam.start_preview()
-            cam.take_photo(self.path + "/" + detection.time + ".jpg")
+            cam.take_photo(self.path + "/" +filename_prefix + ".jpg")
             cam.stop_preview()
         except Exception as e:
             print('Trying to take picture but got error ' + str(e))
